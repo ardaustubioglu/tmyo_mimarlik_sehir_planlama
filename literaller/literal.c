a6f3075e2431d8e24e7b6007ffd2b5bb0ff1da65
Arda ÜSTÜBİOĞLU <ardaustubioglu@gmail.com>
2021-04-10 23:22:43 +0300

    Create literal.c

---
+++ literaller/literal.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main()
+{
+	//LÝTERALLER - LITERALS - RAKAMSAL BÝLGÝLER - SABÝT RAKAMLAR
+	int i;
+	float r;
+	i = 25; 	//25, int deðiþken türünde bir literaldir
+	r = 17.2; 	//17.2 float deðiþken türünde bir literaldir
+	printf("i = %d\n",i);
+	printf("r = %.2f\n",r);	
+}
