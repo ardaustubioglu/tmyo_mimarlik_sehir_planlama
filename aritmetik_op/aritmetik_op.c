f902615596ec186138f1db83751cf92a3108044c
Arda ÜSTÜBİOĞLU <ardaustubioglu@gmail.com>
2021-04-10 23:22:27 +0300

    Create aritmetik_op.c

---
+++ aritmetik_op/aritmetik_op.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main()
+{
+	int sayi1, sayi2;
+	sayi1 = 157;
+	sayi2 = 36;
+	printf("%d + %d = %d \n",sayi1, sayi2, sayi1+sayi2);
+	printf("%d - %d = %d \n",sayi1, sayi2, sayi1-sayi2);
+	printf("%d * %d = %d \n",sayi1, sayi2, sayi1*sayi2);
+	printf("%d / %d = %d \n",sayi1, sayi2, sayi1/sayi2);
+	printf("%d mod %d = %d \n",sayi1, sayi2, sayi1%sayi2);
+}
