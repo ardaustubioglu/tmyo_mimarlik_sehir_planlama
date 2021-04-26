7ee22b1525829ff8c296fc1482c5dcad79dcf0f5
Arda ÜSTÜBİOĞLU <ardaustubioglu@gmail.com>
2021-04-26 12:41:00 +0300

    Create printf_uyg.c

---
+++ printf_uyg/printf_uyg.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	// Düz metin - Literal string
+	printf("1.Merhaba arkadaslar");
+	// Kaçýþ (Kontrol) Karakterleri - Escape Sequences
+	printf("2.Merhaba arkadaslar \n");
+	printf("2.Nasilsiniz? \a");	
+	// Conversion Specifier ( Dönüþüm belirleyicisi )	
+}
